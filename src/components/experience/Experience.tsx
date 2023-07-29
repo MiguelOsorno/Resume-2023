fdf42ebb45b0e49b297902da6b597021ddb8beb2
MiguelOsorno <miguel.osornolinares@gmail.com>
2023-07-29 17:44:36 -0600

    refactor(src/components/experience/experience): update experience

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -31,11 +31,17 @@ export const Experience = () => {
                                 <div className="bg-[#191827] h-8 w-8 flex justify-center items-center rounded-full overflow-hidden">
                                     <img className="w-full h-full object-cover" src="./fuego-logo.jpeg" alt="mipriv-logo" />
                                 </div>
-                                <h4>Fuego</h4>
+                                <h4>Fuego Oct 2022 - Currently</h4>
                             </div>
+                            <p className='mt-4'>In charge of Develop aplications to create expresso on ways never seen before.</p>
                             <div className="mt-4">
                                 <ul className="list-disc">
-                                    <li>Development of Dashboard for expresso machine.</li>
+                                    <li>Development of aplications for expresso machine. <a className='underline text-pink-500' href="https://www.kickstarter.com/projects/meticulous/meticulous-espresso/description" target='_blank'>Meticulous</a></li>
+                                </ul>
+                                <ul className='list-disc'>
+                                    <li>
+                                        Development of animations and transitions to friendly and intuitive user's interface.
+                                    </li>
                                 </ul>
                             </div>
                         </motion.div>
@@ -65,13 +71,14 @@ export const Experience = () => {
                                 <div className="bg-[#191827] h-8 w-8 flex justify-center items-center rounded-full overflow-hidden">
                                     <img className="w-full h-full object-cover" src="./priv.png" alt="mipriv-logo" />
                                 </div>
-                                <h4>Priv Social Inc.</h4>
+                                <h4>Priv Social Inc Feb 2021 - Oct 2022</h4>
                             </div>
+                            <p className='mt-4'>Development of new website version and integration of new features.</p>
                             <div className="mt-4">
                                 <ul className="list-disc">
-                                    <li>Development of Landing page.</li>
-                                    <li>Integration of payment processor such as paypal and conekta.</li>
-                                    <li>Development of new website version using MERN Stack.</li>
+                                    <li>Development of Landing page which means an increase imcome of 20k USD every month.</li>
+                                    <li>Integration of payment processor such as paypal, which means an increase in month revenue over 10%</li>
+                                    <li>Development of new website version.</li>
                                 </ul>
                             </div>
                         </motion.div>
@@ -99,12 +106,12 @@ export const Experience = () => {
                                 <div className="bg-[#191827] h-8 w-8 flex justify-center items-center rounded-full overflow-hidden">
                                     <img className="w-full h-full object-cover" src="./stackcode.jpeg" alt="mipriv-logo" />
                                 </div>
-                                <h4>Stackcode</h4>
+                                <h4>Stackcode Dic 2019 - Feb 2021</h4>
                             </div>
                             <div className="mt-4">
                                 <ul className="list-disc">
-                                    <li>Development of Dashboard for a restaurant using AngularJS.</li>
-                                    <li>Development of websites using Angular 10, JS, CSS3, JQuery</li>
+                                    <li>Development of admin panel for a restaurant to allow have a control over different types of orders and users in the business</li>
+                                    <li>Development of websites to increase the reach of the target audience. Establishments such as construction, opticians</li>
                                 </ul>
                             </div>
                         </motion.div>
